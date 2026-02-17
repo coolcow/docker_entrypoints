d5bc3f8418d10277723623bdd530d6ca6ff827c2
coolcow <mail@coolcow.org>
2026-02-17 08:27:24 +0000

    fix: update entrypoint scripts to source ensure_user_group_home.sh correctly

--- build/entrypoint_crond.sh
+++ build/entrypoint_crond.sh
@@ -6,13 +6,8 @@ if [ -z $CROND_CRONTAB ]; then
     exit 1
 fi
 
-# create user group and home
-/ensure_user_group_home.sh
+. /usr/local/bin/ensure_user_group_home.sh
 
-# configure and exec cron deamon
-crontab \
-  -u $TARGET_USER \
-  $CROND_CRONTAB
+crontab -u $TARGET_USER $CROND_CRONTAB
 
-crond \
-  $CROND_PARAMS
+crond $CROND_PARAMS
--- build/entrypoint_su-exec.sh
+++ build/entrypoint_su-exec.sh
@@ -4,10 +4,8 @@ ENTRYPOINT_COMMAND=$1
 shift
 ENTRYPOINT_PARAMS=$@
 
-# create user group and home
-/ensure_user_group_home.sh
+. /usr/local/bin/ensure_user_group_home.sh
 
-# exec ENTRYPOINT_COMMAND as user
 su-exec \
   $TARGET_USER \
   $ENTRYPOINT_COMMAND \
